5299f05534e9e5b64aad98374335fda7fb9dc809
Ra <ranilsd86@gmail.com>
2023-10-02 17:39:13 -0400

    Finalized project pushgit add .!

---
+++ README.md
@@ -0,0 +1,47 @@
+# quickie_logo_generator
+
+  
+
+  ## Table of Contents
+  * [Description](#description)
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Licenses](#licenses)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+  * [Credits](#credits)
+
+  ## Description
+  This is a command line application where a quick and simple logo is generated as an SVG file. All you have to do is answer the questions that you are prompted with, and it will be created and appear in your dist folder.
+
+  ## Installation
+  To use this command line application you must have VSCode, GitBah, Node.js, Inquirer8.2.4, and Jest installed and running on your machine. Once that is done go to GitHub and then clone this repo to your machine.
+
+  ## Usage
+  - Once this repo is cloned onto your machine..
+  - Make sure all the node modules are installed and activated by running "npm i @inquirer8.2.4" in your command line.
+  - Then  in the command line simply type "node index.js".
+  - Then take your time and answer all the questions that you are promted with.
+  - Once completed the logo.svg file will appear in your "dist" folder.
+  - Open the file in a live browser to view its appearance!
+
+  
+
+  ## Contributing
+  Others can contribute to this by adding more testing, different templates to choose from, also different fonts. That way the logo can be more personalized and not just a base to work on.
+
+  ## Tests
+  The test written are just to see if the shape is rendered and if the color fills the shape.
+
+  ## Demo
+
+
+  ## Questions
+  Have questions about this project?  
+  GitHub: https://github.com/RanilSD  
+  Email: ranilsd86@gmail.com
+
+  ## Credits
+  Ranil Dissanayaka
+
---
+++ index.js
@@ -0,0 +1,96 @@
+//defining the constants
+const fs = require("fs");
+const inquirer = require("inquirer");
+const { Triangle, Circle, Square } = require("./lib/shapes");
+
+//questions to create logo
+
+const inputQuestion = [
+    {
+        name: "shape",
+        type: "list",
+        message: "Please choose a shape:",
+        choices: ["Triangle", "Circle", "Square"],
+    },
+    {
+        name: "shapeColor",
+        type: "input",
+        message: "Please enter Shape color(or hexadecimal number):",
+    },
+    {
+        name: "text",
+        type: "input",
+        message: "Please enter text for logo up to 3 characters:",
+        validate: (input) => input.length >= 1 && input.length <= 3
+    },
+    {
+        name: "textColor",
+        type: "input",
+        message: "Please enter a color for text(or hexadecimal number):",
+    },
+];
+
+
+//writing the file dependant on the answers of prompted inputQuestions
+
+function writeFile(fileName, data) {
+    return new Promise((resolve, reject) => {
+        fs.writeFile('./dist/logo.svg', data, (err) => {
+            if (err) {
+                console.error(`Error writing to ${fileName}: ${err}`);
+            } else {
+                console.log(`Generated to ${fileName}`);
+            }
+            resolve({
+                ok: true,
+                message: console.log('Success! Navigate to the "dist" folder to see your logo!')
+            });
+        })
+    });
+}
+
+
+//defining the logo file name
+
+const logoFile = "logo.svg";
+
+//creating the logo from user input
+
+async function generateLogo() {
+    const userInput = await inquirer.prompt(inputQuestion);
+    const userText = userInput.text;
+    const userTextColor = userInput.textColor;
+    const userShapeColor = userInput.shapeColor;
+    const userShapeType = userInput.shape;
+
+    let createdShape;
+    if (userShapeType === "Square") {
+        createdShape = new Square(userShapeColor, userTextColor, userText);
+        console.log("Square selected");
+    } else if (userShapeType === "Triangle") {
+        createdShape = new Triangle(userShapeColor, userTextColor, userText);
+        console.log("Triangle selected");
+    } else if (userShapeType === "Circle") {
+        createdShape = new Circle(userShapeColor, userTextColor, userText);
+        console.log("Circle selected");
+    } else {
+        console.log("Error selecting shape!");
+    }
+    console.log("Shape Color: " + userShapeColor);
+    console.log("Text: " + userText);
+    console.log("Text Color: " + userTextColor);
+
+    createdShape.shapeColor = userShapeColor;
+    createdShape.textColor = userTextColor;
+    createdShape.text = userText;
+
+
+    const svgString = createdShape.render();
+
+    //writing logo to svg file
+    writeFile(logoFile, svgString);
+
+
+}
+
+generateLogo();
